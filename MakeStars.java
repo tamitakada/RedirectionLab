66046d25b46dee6f7bc955efddb77563a76ed0b0
Tami Takada <tami@mytalos.com>
2020-12-23 14:06:31 -0500

    added a test file + made stars class

---
+++ MakeStars.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class MakeStars {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    while (scan.hasNextLine()) {
+      System.out.println(scan.nextLine());
+    }
+  }
+}
